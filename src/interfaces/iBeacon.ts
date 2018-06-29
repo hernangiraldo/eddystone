b414d4cad3c707ac9a817b003078774c60d16447
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2018-06-29 16:21:52 -0400

    Eddystone plugin config

---
+++ src/interfaces/iBeacon.ts
@@ -0,0 +1,5 @@
+export interface iBeacon {
+  uid: string;
+  instanceID: string;
+  distance: number
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { iBeacon } from './../../interfaces/iBeacon'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  beacons: iBeacon[];
 
+  startScan() {
+    evothings.eddystone.startScan(
+      beacon => {
+        let distance = evothings.eddystone.calculateAccuracy( beacon.txPower, beacon.rssi );
+        let tempBeacon: iBeacon = {
+          uid: this.toHexString( beacon['nid'] ? beacon['nid'] : '' ).toUpperCase(),
+          instanceID: this.toHexString( beacon['bid'] ? beacon['bid'] : '' ).toUpperCase(),
+          distance: distance
+        };
+        if (tempBeacon.instanceID) {
+          console.log(tempBeacon);
+        }
+      },
+      (error) => {
+        console.log(JSON.stringify(error));
+      }
+    );
+  }
+
+  toHexString(byteArray) {
+    return Array.from(byteArray, (byte: any) => {
+      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
   }
 
 }
